543737987ccfd886bd2acf85ce103a1fb7059a67
Nessaj44 <johnedmoncoc@gmail.com>
2023-08-25 14:32:41 +0800

    Add form validation input  style

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const form = document.getElementById("subcription--form");
+const emailError = document.getElementById("email--error");
+const emailInput = document.getElementById("email");
+
+
+form.addEventListener("submit", (event) => {
+  emailInput.classList.remove("error");
+  emailError.style.visibility = "hidden";
+
+  if(!emailInput.checkValidity()) {
+    event.preventDefault();
+    emailInput.classList.add('error');
+    emailError.style.visibility = "visible";
+  }
+});
